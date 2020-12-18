b259ef7c234b0d7734beeddde7ff67a7dc971a2f
julianamonr03 <julianamonroy03@hotmail.com>
2020-12-18 01:46:36 +0000

    Error in function [solved]

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(roman_string):
     if type(roman_string) == str:
         sum_v = 0
         num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
