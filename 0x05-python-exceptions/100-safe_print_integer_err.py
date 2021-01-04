2f26f56f48a8d7ca2504d921dee85b1dfe24ab27
julianamonr03 <julianamonroy03@hotmail.com>
2021-01-04 21:36:19 +0000

    Correction print error message in stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except (TypeError, ValueError):
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
         return (False)
 
     return (True)
