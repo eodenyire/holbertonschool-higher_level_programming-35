2079f62bf6050d69175b782a96cabc8c6fdec978
julianamonr03 <julianamonroy03@hotmail.com>
2021-01-19 23:48:51 +0000

    indents problem fix

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,7 +17,5 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """ Return str """
-        return("[{:s}] {}/{}".
-            format(self.__class__.__name__,
-                        self.__width,
-                        self.__height))
+        return("[{}] {}/{}".format(
+            self.__class__.__name__, self.__width, self.__height))
