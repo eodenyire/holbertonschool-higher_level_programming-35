b3baab05eb66e9e6623914c135f94f31fc22cb95
julianamonr03 <julianamonroy03@hotmail.com>
2021-01-21 01:17:22 +0000

    script that adds all arguments to a Python list, and then save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Load, add, save  """
+import sys
+
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+def class_to_json(obj):
+    """ Script that adds all arguments to a Python list,
+    and then save them to a file """
+    try:
+        add_i = load_from_json_file('add_item.json')
+        save_to_json_file(add_i + argv[1:], 'add_item.json')
+    except Exception:
+        save_to_json_file(argv[1:], 'add_item.json')
