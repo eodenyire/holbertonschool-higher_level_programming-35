eadba10c2bad21da41d58aa8b5172b584b0e2b4f
julianamonr03 <julianamonroy03@hotmail.com>
2021-01-21 01:23:31 +0000

    script that adds all aeguments to a python list [UPDATE] - fix error with unknow function

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """ Load, add, save  """
-import sys
+from sys import argv
 
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-
-def class_to_json(obj):
-    """ Script that adds all arguments to a Python list,
-    and then save them to a file """
-    try:
-        add_i = load_from_json_file('add_item.json')
-        save_to_json_file(add_i + argv[1:], 'add_item.json')
-    except Exception:
-        save_to_json_file(argv[1:], 'add_item.json')
+""" Script that adds all arguments to a Python list,
+and then save them to a file """
+try:
+    add_i = load_from_json_file('add_item.json')
+    save_to_json_file(add_i + argv[1:], 'add_item.json')
+except Exception:
+    save_to_json_file(argv[1:], 'add_item.json')
