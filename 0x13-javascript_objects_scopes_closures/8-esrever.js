9c3120a8b23f604ef9fa0dc22a3d8b9c036decf0
julianamonr03 <julianamonroy03@hotmail.com>
2021-03-23 19:51:40 -0500

    [UPDATE] - Function that reverse a list - fix first line code

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,10 +1,8 @@
-#!/usr/bin/node
-
+#!/usr/bin/env node
 exports.esrever = function (list) {
   const revsList = [];
-  let i = 0;
 
-  for (i = list.lenght - 1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     revsList.push(list[i]);
   }
   return revsList;
