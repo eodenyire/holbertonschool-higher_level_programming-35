c363a6e8a36891d071cf9014a9514f97e090a2b1
julianamonr03 <julianamonroy03@hotmail.com>
2021-03-28 22:07:03 -0500

    [UPDATE] - changes in line 15, 17

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,9 +12,9 @@ if __name__ == '__main__':
                          passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
-    cur_obj.execute("SELECT cities.name FROM cities\
-                     INNER JOIN states ON cities.state_id\
-                          = states.id WHERE states.name = %s", [argv[4]])
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
+                INNER JOIN states ON cities.state_id = states.id\
+                WHERE states.name = %s ORDER BY cities.id ASC", [argv[4]])
 
     rows = cur.fetchall()
     j = []
