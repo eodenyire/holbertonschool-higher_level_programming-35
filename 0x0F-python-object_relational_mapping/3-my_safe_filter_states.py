a49885f74ac6b50275703092bf98e29a5a162ce9
julianamonr03 <julianamonroy03@hotmail.com>
2021-03-28 21:05:53 -0500

    [UPDATE] - fix line 20 execute problem

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = %s\
-                ORDER BY id ASC".format(argv[4]))
+    n = "SELECT * FROM states WHERE name = %s ORDER BY id ASC".format(argv[4])
+    cur.execute(n)
 
     rows = cur.fetchall()
     for i in rows:
